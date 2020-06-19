6bf64d728a339a423f803b6d0adeab652f44e32c
kyle <kyle@flexo.local>
2020-06-19 09:22:28 -0400

    server integration

--- server.js
+++ server.js
@@ -1,14 +1,47 @@
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
+const bodyParser = require('body-parser');
+const ledshim = require('./ledshim');
+const log = require('./simple-logger').getLogger('Server');
+const port = process.env.PORT || 80;
+const ledsReady = { r: 50, g: 0, b: 0, n: [27] };
+const serverReady = { r: 0, g: 25, b: 0, n: [26] };
+let leds;
+
+app.use(bodyParser.json());
 
 // Get status
 app.get('/status', (req, res) => {
-  res.send({ MOCHA: process.env.MOCHA || 'nothing' });
+  res.send({ status: 'ready' });
 });
 
-// Start the server
-const port = process.env.PORT || 80;
-server.listen(port, callback => {
-  console.info(`Server running on port ${port}`);
+// Update LED indicator
+app.post('/leds', async (req, res) => {
+  log.info('LED update request2: ', JSON.stringify(req.body));
+  await leds.setPixels(req.body);
+  res.send({ op: 'success' });
+});
+
+// Start the service
+ledshim.init().then(device => {
+  leds = device;
+  leds.setPixels([ledsReady]).then(() => {
+    server.listen(port, callback => {
+      log.info(`Server running on port ${port}`);
+      leds.setPixels([ledsReady, serverReady]);
+    });
+  });
+});
+
+// Termination event
+process.on('SIGINT', function () {
+  log.warn('Service shutting down!');
+
+  if (leds) {
+    leds.setPixels([]).then(() => setTimeout(process.exit, 100));
+  }
+  else {
+    process.exit();
+  }
 });
